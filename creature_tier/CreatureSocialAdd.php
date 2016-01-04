034708d9cf5f2b4180d687c71b923da63e03bd4d
Mark N Broadhead <marknbroadhead@gmail.com>
2016-01-03 19:50:50 -0700

    Implement creature social add page

---
+++ creature_tier/CreatureSocialAdd.php
@@ -0,0 +1,33 @@
+<?php
+include '../config.php';
+try{
+    $database = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    print 'Connection failed: ' . $e->getMessage();
+}
+
+$errorCount = 0;
+if (!isset($_POST['Name']) || strlen($_POST['Name']) < 1) {
+    print "<p>Please specify a name for the creature social type</p>";
+    $errorCount++;
+}
+if (strlen($_POST['Name']) > 50) {
+    print "<p>The creatures social type's name must be shorter than 50 characters</p>";
+    $errorCount++;
+}
+
+
+if ($errorCount == 0) {
+    $query = "
+        INSERT INTO creature_tier_social (name)
+        VALUES (:name);
+    ";
+    $stmt = $database->prepare($query);
+    $stmt->execute(array(':name' => $_POST['Name']));
+
+    if($stmt->errorInfo()[0] == "00000") {
+        print "Social type " . $_POST['Name'] . " successfully added to the Database";
+    } else {
+        print "Error Adding social type " . $_POST['Name'];
+    }
+}
---
+++ creature_tier/CreatureSocialEdit.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Attribute Type Edit</title>
+    <link rel="stylesheet" href="../css/normalize.min.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js">
+    <script type="text/javascript" src="../js/main.js"></script>
+</head>
+<body>
+<main>
+    <h1>Attribute Type Edit</h1>
+<?php
+include '../config.php';
+try{
+    $database = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    print 'Connection failed: ' . $e->getMessage();
+}
+?>
+    <div>
+        <h2>Add new creature social type </h2>
+        <form method="post" action="CreatureSocialAdd.php">
+            <input type="text" name="Name">
+            <input type="submit" value="Create">
+            <input type="reset" value="Restart">
+        </form>
+    </div>
+    <div>
+        <h2>List of Creature Social Types</h2>
+        <ul>
+        <?php
+        $query = "
+            SELECT name FROM creature_tier_social
+        ";
+        $stmt = $database->query($query);
+        $attributes = $stmt->fetchAll();
+        foreach ($attributes as $attribute) {
+            print "<li>" . $attribute['name'] . "</li>";
+        }
+        ?>
+        </ul>
+    </div>
+</main>
+</body>
+</html>
