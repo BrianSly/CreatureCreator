f8ed855a9752efeb968a4322c8afb3c251ae43c9
Brian Sly <Brian.H.Sly@gmail.com>
2016-01-04 19:08:14 -0700

    Add tag so ClassEdit.php tab would behave similarly to the other pages.

--- ClassEdit.php
+++ ClassEdit.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Class Edit</title>
     <link rel="stylesheet" href="css/normalize.min.css">
     <link rel="stylesheet" href="css/main.css">
+
 </head>
 <body>
 <main>
