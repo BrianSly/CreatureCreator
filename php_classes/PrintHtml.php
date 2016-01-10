908f7f13dbb50026bf317af618289044fe617c4f
Mark N Broadhead <marknbroadhead@gmail.com>
2016-01-10 13:36:53 -0700

    Add class to print html before and in head tag

---
+++ php_classes/PrintHtml.php
@@ -0,0 +1,26 @@
+<?php
+namespace BattleChores;
+
+class PrintHtml
+{
+    /**
+     * @param string $title page title
+     * @return string header html
+     */
+    public function head($title)
+    {
+        $header = '<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>' . $title . '</title>
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</head>';
+        return $header;
+    }
+}
