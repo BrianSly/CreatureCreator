8c0321129e7b63ff6a13c10f2d9e2e48c5a035ae
Mark N Broadhead <MarkNBroadhead@gmail.com>
2016-01-11 22:40:00 -0700

    Fix beader code to work in any directory

--- php_classes/PrintHtml.php
+++ php_classes/PrintHtml.php
@@ -16,10 +16,10 @@ class PrintHtml
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>' . $title . '</title>
-    <link rel="stylesheet" href="css/normalize.min.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="/css/normalize.min.css">
+    <link rel="stylesheet" href="/css/main.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script type="text/javascript" src="js/main.js"></script>
+    <script type="text/javascript" src="/js/main.js"></script>
 </head>';
         return $header;
     }
